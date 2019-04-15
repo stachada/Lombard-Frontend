36295fe369f61e2f85121f2464ae0701f88650f9
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-04-15 13:21:23 +0200

    Initial form for items

--- LombardClient/src/app/app.module.ts
+++ LombardClient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemFormComponent } from './item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
