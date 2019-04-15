79a7260d4ec2ed32f5b7fbd74b52e1184a8a4774
Bartłomiej Kacperski <bartlomiejkacperski7734@gmail.com>
2019-04-15 15:27:44 +0200

    Merge branch 'master' of https://github.com/LeszekBlazewski/Lombard-Frontend

---
+++ LombardClient/src/app/services/serviceBase.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ServiceBase {
+    BASE_URL = 'http://localhost:64905/'
+
+    constructor(private http: HttpClient) {}
+
+    config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    get(relativeUrl: string): Observable<any> {
+        const url = this.BASE_URL + relativeUrl;
+        return this.http.get(url);
+    }
+
+    post(relativeUrl: string, object: object) {
+        const url = this.BASE_URL + relativeUrl;
+        return this.http.post(url, JSON.stringify(object), this.config);
+    }
+}
