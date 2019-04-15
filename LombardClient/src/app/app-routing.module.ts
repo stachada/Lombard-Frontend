df748920698eea4d78a69329796224a7f5e17a37
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-04-15 20:59:14 +0200

    Added routes, and basic rendering of product

--- LombardClient/src/app/app-routing.module.ts
+++ LombardClient/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './components/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "items", component: ItemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
